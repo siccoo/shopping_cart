661ee215e0e9d16dcab7284b8d49138222834bc1
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-11-09 15:43:40 +0100

    updates on productActions.ts

--- src/actions/productActions.ts
+++ src/actions/productActions.ts
@@ -1,7 +1,13 @@
-import { PRODUCT_LIST_REQUEST } from "../constants/productConstants";
+import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
 
-export const listProducts = () => (dispatch: (arg0: { type: string }) => void) => {
+export const listProducts = () => async (dispatch: (arg0: { type: string }) => void) => {
     dispatch({
         type: PRODUCT_LIST_REQUEST
-    })
+    });
+    try {
+        const { data } = await fetch("product.js");
+        dispatch({ type: PRODUCT_LIST_SUCCESS, product: data })
+    } catch {
+        dispatch({ type: PRODUCT_LIST_FAIL })
+    }
 }
